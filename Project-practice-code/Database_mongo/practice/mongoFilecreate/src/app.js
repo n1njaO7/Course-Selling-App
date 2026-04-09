1185915167ec38470be6f265393dbf3715bf6225
n1njaO7 <n1njasri07@gmail.com>
2026-04-09 06:05:11 +0530

    setup sign in endpoint

--- Project-practice-code/Database_mongo/practice/mongoFilecreate/src/app.js
+++ Project-practice-code/Database_mongo/practice/mongoFilecreate/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 const {UserModel,TodoModel} = require("./db");
-
+const {jwt , JWT_SECRET} = require("./auth")
 app.use(express.json())
 
 app.post("/signup",async(req,res)=>{
@@ -20,8 +20,30 @@ app.post("/signup",async(req,res)=>{
 
 })
 
-app.post("/signin",(req,res)=>{
+app.post("/signin",async(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        email:email,
+        password:password
+    })
     
+    if(user){
+        const token = jwt.sign({
+            userId : user._id.toString()
+        },JWT_SECRET);
+
+        res.status(200).json({
+            message : "You are signed in",
+            token : token 
+        })
+    }
+    else{
+        res.status(403).json({
+            message : "wrong credentials"
+        })
+    }
 })
 
 app.post("/todo",(req,res)=>{
--- Project-practice-code/Database_mongo/practice/mongoFilecreate/src/auth.js
+++ Project-practice-code/Database_mongo/practice/mongoFilecreate/src/auth.js
@@ -2,5 +2,10 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = "asdfwuyfgiwgjk35dfa2";
 
 function auth(req,res,next){
-    
+
+}
+
+module.exports={
+    jwt,
+    JWT_SECRET
 }
