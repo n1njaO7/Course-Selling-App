3fb7b73beaec39af484ed41cc2fd99a3f5490f04
n1njaO7 <n1njasri07@gmail.com>
2026-04-10 23:55:31 +0530

    cretreted several endpoints

--- Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
+++ Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
@@ -1,4 +1,22 @@
 const express = require ("express");
 const app = express();
 
+app.use(express.json())
+
+app.post("./signup",(req,res)=>{
+
+})
+
+app.post("./signup",(req,res)=>{
+    
+})
+
+app.get("./",(req,res)=>{
+    
+})
+
+app.get("./me",(req,res)=>{
+    
+})
+
 module.exports= app;
