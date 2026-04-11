3fd8c204d1203bbd1da37052a904103f5cd68493
n1njaO7 <n1njasri07@gmail.com>
2026-04-12 00:11:36 +0530

    applied zod

--- Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
+++ Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
@@ -1,33 +1,56 @@
 const express = require ("express");
 const app = express();
 const {z} = require("zod");
+const {UserModel,TodoModel} =  require("./db");
+const { default: mongoose } = require("mongoose");
 
 app.use(express.json())
 
-app.post("./signup",(req,res)=>{
+mongoose.connect("")
+
+app.post("/signup",async (req,res)=>{
 
     const inputVal = z.object({
         name : z.string().min(3).max(20),
         email : z.string().min(3).max(50).email(),
         password : z.string().min(6).max(30),
     })
-
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-
+    
+    const parsedData = inputVal.safeParse(req.body)
+    if(!parsedData.success){
+        return res.status(400).json({
+            message : "wrong input",
+            error : parsedData.error
+        })
+    }
+    
+    try{
+        const {name , email, password} = parsedData.data
+        await UserModel.create({
+        name : name,
+        email: email,
+        password : password
+        })
+        res.status(203).json({
+            message: "You sre Signed up"
+        })
+    }catch(e){
+        res.status(403).json({
+            meaasge : e
+        })
+    }
 
 })
 
-app.post("./signup",(req,res)=>{
+app.post("/signup",(req,res)=>{
     
 })
 
-app.get("./todo",(req,res)=>{
+app.get("/todo",(req,res)=>{
     
 })
 
-app.get("./todos",(req,res)=>{
+app.get("/todos",(req,res)=>{
     
 })
 
