86d637ade716690d587dd89952992f540e6ac427
n1njaO7 <n1njasri07@gmail.com>
2026-04-11 19:20:27 +0530

    intsalled and setup zod lib

--- Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
+++ Project-practice-code/Database_mongo/dBwithZodBycrpt/src/app.js
@@ -1,9 +1,21 @@
 const express = require ("express");
 const app = express();
+const {z} = require("zod");
 
 app.use(express.json())
 
 app.post("./signup",(req,res)=>{
+
+    const inputVal = z.object({
+        name : z.string().min(3).max(20),
+        email : z.string().min(3).max(50).email(),
+        password : z.string().min(6).max(30),
+    })
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
 
 })
 
