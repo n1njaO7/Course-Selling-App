80857bb6c1e21e1e67e82e999900a1a9c614415f
n1njaO7 <n1njasri07@gmail.com>
2026-04-11 19:07:48 +0530

    created auth js file

---
+++ Project-practice-code/Database_mongo/dBwithZodBycrpt/src/auth.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "jhdgwqyfyq23gdub"
+
+function auth (){
+    const email = req.body.email;
+    const password = req.body.password;
+    try{
+        
+    }
+
+}
