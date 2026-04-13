d40c0e6fffe301527e0be7f637ba0c9ad298a2e7
n1njaO7 <n1njasri07@gmail.com>
2026-04-13 05:46:12 +0530

    created structure couse Router

---
+++ Course-Selling-App/routes/course.js
@@ -0,0 +1,18 @@
+const {Router} = require("express");
+const courseRouter = Router();
+
+courseRouter.post("/purchase",(req,res)=>{
+    res.json({
+        message : "couse Purchase router"
+    })
+})
+
+courseRouter.get("/preview",(req,res)=>{
+    res.json({
+        message : "couse preview router"
+    })
+})
+
+module.exports={
+    courseRouter
+}
