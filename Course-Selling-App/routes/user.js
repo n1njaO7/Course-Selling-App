13be67cbde42b4cbbdd45553aaf1f93e8f29ad4e
n1njaO7 <n1njasri07@gmail.com>
2026-04-13 05:31:34 +0530

    created seprate router folder and structured user router file

---
+++ Course-Selling-App/routes/user.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+const userRouter = Router();
+
+userRouter.post("/signin",(req,res)=>{
+    res.json({
+        message : "User signin Router"
+    })
+})
+
+userRouter.post("/signup",(req,res)=>{
+    res.json({
+        message : "User signup Router"
+    })
+})
+
+userRouter.get("/purchases",(req,res)=>{
+    res.json({
+        message : "User signin Router"
+    })
+})
+
