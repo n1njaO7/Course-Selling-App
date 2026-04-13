e8947dae08ceff068819ffbacda73372fc5fc810
n1njaO7 <n1njasri07@gmail.com>
2026-04-13 06:47:29 +0530

    created and structured db file

---
+++ Course-Selling-App/src/db.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const UserSchema = new Schema({
+    userId : ObjectId,
+    firstName  : String,
+    lastName  : String,
+    email : {type:String, unique : true},
+    password : String,
+})
+
+const AdminSchema = new Schema({
+    adminId : ObjectId,
+    firstName  : String,
+    lastName  : String,
+    email : {type:String, unique : true},
+    password : String,
+})
+
+const CourseSchema = new Schema({
+    courseId : ObjectId,
+    title : String,
+    discription : String,
+    price : Number,
+    imgUrl : String,
+    creatorUrl : ObjectId
+})
+
+const PurchaseSchema = new Schema({
+    purchaseId : ObjectId,
+    userId : ObjectId,
+    courseId : ObjectId,
+})
+
+const UserModel = mongoose.model("user",UserSchema);
+const AdminModel = mongoose.model("admin",AdminSchema);
+const CourseModel = mongoose.model("course",CourseSchema);
+const PurchaseModel = mongoose.model("purchase",PurchaseSchema);
+
+module.exports={
+    UserModel,
+    AdminModel,
+    CourseModel,
+    PurchaseModel
+}
+
+
+
