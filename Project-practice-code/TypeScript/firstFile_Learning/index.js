29d4c514910655c7c881ddf8458f4c1969619a11
n1njaO7 <n1njasri07@gmail.com>
2026-04-18 01:08:35 +0530

    return sum

--- Project-practice-code/TypeScript/firstFile_Learning/index.js
+++ Project-practice-code/TypeScript/firstFile_Learning/index.js
@@ -5,7 +5,8 @@ function name(firstName) {
 }
 name("Ashmit");
 function sum(a, b) {
-    console.log(a + b);
+    return a + b;
 }
-sum(4, 5);
+let ans = sum(4, 5);
+console.log(ans);
 //# sourceMappingURL=index.js.map
