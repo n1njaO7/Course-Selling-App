4b646dc0e4cf0e80c466c55fdb9d41518702290b
Ashmit Srivastava <157968662+n1njaO7@users.noreply.github.com>
2026-06-21 06:05:48 +0530

    Refactor app.js for cleaner middleware usage

--- Course-Selling-App/src/app.js
+++ Course-Selling-App/src/app.js
@@ -1,19 +1,20 @@
-const express = require ("express");
-const {userRouter} =require("../routes/user");
-const {adminRouter} =require("../routes/admin");
-const {courseRouter} =require("../routes/course");
+const express = require("express");
+const { userRouter } = require("../routes/user");
+const { adminRouter } = require("../routes/admin");
+const { courseRouter } = require("../routes/course");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(cookieParser());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Course Selling App Backend is running 🚀");
+});
 
-app.use(express.json()); 
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/course", courseRouter);
 
-app.use("/user",userRouter);
-app.use("/admin",adminRouter);
-app.use("/course",courseRouter);
-
-
-module.exports= app
+module.exports = app;
